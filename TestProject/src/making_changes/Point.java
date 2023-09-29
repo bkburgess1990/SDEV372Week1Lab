eb251f14520f9d7aaeb27be1e0c72e980f3a1d1a
bkburgess1990 <burgess.brandon@student.greenriver.edu>
2023-09-29 10:34:15 -0700

    Added getters/setters and toString methods to Point class

--- TestProject/src/making_changes/Point.java
+++ TestProject/src/making_changes/Point.java
@@ -10,5 +10,27 @@ public class Point {
         this.y = y;
     }
 
+    public double getX() {
+        return x;
+    }
 
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
